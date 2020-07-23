d8b632c249964be1a814bbc2f2a249b170c1cf1a
austindickey <austindickey@me.com>
2020-07-22 19:20:07 -0500

    adjusting view height for mobile devices watching live stream

--- js/twitch-embed.js
+++ js/twitch-embed.js
@@ -1,5 +1,11 @@
 let viewWidth = ($(window).width() * .9)
-let viewHeight = (viewWidth * .562)
+let viewHeight
+
+if ($(window).width() > 480) {
+    viewHeight = (viewWidth * .562)
+} else {
+    viewHeight = (viewWidth * 1.25)
+}
 
 new Twitch.Embed("twitch-embed", {
     width: viewWidth,
